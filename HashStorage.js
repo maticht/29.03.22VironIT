c356bf0fe3eca0e1613e62750ee5588322d0bdac
maticht <74956969+maticht@users.noreply.github.com>
2022-03-31 12:52:44 +0300

    29.03.22VironIT
    29.03.22VironIT

--- HashStorage.js
+++ HashStorage.js
@@ -15,7 +15,6 @@ class HashStorage {
         return Object.keys(this.drinksCollection)
     }
 }
-
 function addBut(){
     let name = window.prompt('Напишите название коктейля')
     let alcoholism = window.prompt('Он алкогольный?')
@@ -26,7 +25,6 @@ function addBut(){
     stringCocktailObj = stringCocktailObj.replace(/,/g,'').replace(/""/g,'\n').replace(/{/g,'').replace(/}/g,'').replace(/"/g,' ')
     alert(`Вы добавили рецепт напитка\n${stringCocktailObj}`)
 }
-
 function deleteBut(){
     const name = window.prompt('Удалить коктейль');
     let stringCocktailObj = JSON.stringify(cocktailStorage.getValue(name));
@@ -34,7 +32,6 @@ function deleteBut(){
     alert(`Удален коктейль:\n${stringCocktailObj}`)
     cocktailStorage.deleteValue(name)
 }
-
 function recipeBut(){
     const name = window.prompt('Получить рецепт напитка...')
     let stringCocktailObj = JSON.stringify(cocktailStorage.getValue(name));
@@ -43,7 +40,6 @@ function recipeBut(){
     let strRecipe = document.createTextNode(`Рецепт напитка:\n${stringCocktailObj}`)
     recipe.appendChild(strRecipe)
 }
-
 function viewAllBut(){
     let stringCocktailObj = JSON.stringify(cocktailStorage.getKeys());
     stringCocktailObj = stringCocktailObj.replace(/,/g,'').replace(/""/g,'\n').replace(/{/g,'').replace(/[\[]/g,'').replace(/[\]]/g,'').replace(/}/g,'').replace(/"/g,' ')
@@ -51,7 +47,16 @@ function viewAllBut(){
     let strAllCocktails = document.createTextNode(`Список всех рецептов:\n${stringCocktailObj}`)
     allCocktails.appendChild(strAllCocktails)
 }
-
 const cocktailStorage = new HashStorage()
-cocktailStorage.addValue('Маргарита', {name:'Маргарита', alcoholism: 'да', list:'Водка Finlandia 50мл; Кофейный ликер 25мл; Лед в кубиках 120 г', description: 'Наполни стакан кубиками льда доверху, затем налей кофейный ликер 25 мл, водку 50 мл и размешай коктейльной ложкой.'})
-cocktailStorage.addValue('Пеликан', {name:'Пеликан', alcoholism: 'нет', list:'Гренадин Monin 10мл; Клубничный сироп Monin 10мл; Персиковый сок 150мл; Лимонный сок 15мл; Банан 110г; Клубника 50г; Дробленый лед 60г', description:'Положи в блендер очищенную и нарезанную половинку банана и клубнику 2 ягоды. Налей лимонный сок 15 мл, гренадин 10 мл, клубничный сироп 10 мл и персиковый сок 150 мл. Добавь в блендер совок дробленого льда и взбей. Перелей в хайбол. Укрась кружком банана и половинкой клубники на коктейльной шпажке.'})
+cocktailStorage.addValue('Маргарита',{
+    name:'Маргарита',
+    alcoholism: 'да',
+    list:'Водка Finlandia 50мл; Кофейный ликер 25мл; Лед в кубиках 120 г',
+    description: 'Наполни стакан кубиками льда доверху, затем налей кофейный ликер 25 мл, водку 50 мл и размешай коктейльной ложкой.'
+});
+cocktailStorage.addValue('Пеликан',{
+    name:'Пеликан',
+    alcoholism: 'нет',
+    list:'Гренадин Monin 10мл; Клубничный сироп Monin 10мл; Персиковый сок 150мл; Лимонный сок 15мл; Банан 110г; Клубника 50г; Дробленый лед 60г',
+    description:'Положи в блендер очищенную и нарезанную половинку банана и клубнику 2 ягоды. Налей лимонный сок 15 мл, гренадин 10 мл, клубничный сироп 10 мл и персиковый сок 150 мл. Добавь в блендер совок дробленого льда и взбей. Перелей в хайбол. Укрась кружком банана и половинкой клубники на коктейльной шпажке.'
+});
